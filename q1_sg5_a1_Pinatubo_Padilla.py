695edeabb67808fc67d3f40083546cd977d09596
knoah0116 <kdpadilla2030@clc.pshs.edu.ph>
2026-09-18 07:58:23 +0800

    Create q1_sg5_a1_Pinatubo_Padilla.py

---
+++ q1_sg5_a1_Pinatubo_Padilla.py
@@ -0,0 +1,16 @@
+class Hero:
+    def __init__(self,name,hp):
+        self.name=name
+        self.hp=hp
+        
+        
+    def take_damage(self,amount):
+        self.hp=self.hp-amount
+        
+Arthur=Hero("Arthur",100)
+Morgana=Hero("Morgana",100)
+
+Arthur.take_damage(10)
+
+print("Arthur's hp:",Arthur.hp)
+print("Morgana's hp:",Morgana.hp)
